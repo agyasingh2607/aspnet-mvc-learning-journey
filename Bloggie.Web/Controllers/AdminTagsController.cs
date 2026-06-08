f6330f01c93bd1aa020234647f1882210c407404
agyasingh2607 <agyasingh2607@gmail.com>
2026-06-08 18:41:06 +0800

    Implement tag creation using manual request handling

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+            return View("Add");
+        }
+    }
+}
