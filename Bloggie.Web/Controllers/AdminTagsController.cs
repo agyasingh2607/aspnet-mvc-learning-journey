5684c000b1f0992a9f62dab1104904a7f12c6e7a
agyasingh2607 <agyasingh2607@gmail.com>
2026-06-15 15:02:17 +0800

    Asynchronous Programming and Repository Pattern

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,21 +1,110 @@
-﻿using Bloggie.Web.Models.ViewModels;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
-        public IActionResult Add()
+
+        //constructor injection of the database context
+        private readonly ITagRepository tagRepository;
+        public AdminTagsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Add()      //no need to make Asynchronous because we are not doing any database operation here, just returning the view
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Add(AddTagRequest addTagRequest)
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
         {
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
-            return View("Add");
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            await tagRepository.AddAync(tag);
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public async Task<IActionResult> List()
+        {
+            //use dbContext to read the tags
+            var tags = await tagRepository.GetAllAsync();
+            return View(tags);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var tag = await tagRepository.GetAsync(id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+            var updatedTag = await tagRepository.UpdateAsync(tag);
+
+            if (updatedTag != null)
+            {
+                // Show a success notification
+            }
+            else
+            {
+                // Show an error notification
+            }
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+
+        }
+
+            [HttpPost]
+            public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+            {
+                var deletadTag = await tagRepository.DeleteAsync(editTagRequest.Id);
+
+                if(deletadTag != null)
+                {
+                    //show success notification
+                    return RedirectToAction("List");
+                }
+
+                //Show an error notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+
+            }
         }
     }
-}
---
+++ Bloggie.Web/Repositories/ITagRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+    public interface ITagRepository
+    {
+        //we have 5 methods in the AdminTagsController,
+        //so we will create 5 methods in this interface as well,
+        //and then we will implement this interface in the AdminTagsController
+
+        Task<IEnumerable<Tag>> GetAllAsync();
+
+        Task<Tag?> GetAsync(Guid id);
+
+        Task<Tag> AddAync(Tag tag);
+
+        Task<Tag?> UpdateAsync(Tag tag);
+
+        Task<Tag?> DeleteAsync(Guid id);
+    }
+}
