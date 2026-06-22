6497daa3cd19f772490f9d76ceeb895c14340a52
agyasingh2607 <agyasingh2607@gmail.com>
2026-06-22 16:55:23 +0800

    Completed BlogPost CRUD functionality

---
+++ Bloggie.Web/Repositories/IBlogPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories
+{
+    public interface IBlogPostRepository
+    {
+        Task<IEnumerable<BlogPost>> GetAllAsync();
+
+        Task<BlogPost?> GetAsync(Guid id);
+
+        Task<BlogPost> AddAync(BlogPost blogPost);
+
+        Task<BlogPost?> UpdateAsync(BlogPost blogPost);
+
+        Task<BlogPost?> DeleteAsync(Guid id);
+    }
+}
